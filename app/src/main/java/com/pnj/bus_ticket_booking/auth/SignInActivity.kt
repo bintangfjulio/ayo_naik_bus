842741c288121fd2b905528ecbf2f886263b2a3a
Bintang Fajar Julio <79626613+bintangfjulio@users.noreply.github.com>
2023-07-13 22:27:10 +0700

    add splash

--- app/src/main/java/com/pnj/bus_ticket_booking/auth/SignInActivity.kt
+++ app/src/main/java/com/pnj/bus_ticket_booking/auth/SignInActivity.kt
@@ -3,7 +3,6 @@ package com.pnj.bus_ticket_booking.auth
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
