26c2b0ee96c4a4a74303ba0414ccb103119bfbb4
Muhamad Dwiki Hermansyah <dwikihermansyah17@gmail.com>
2023-07-17 15:53:08 +0700

    menambahkan fitur pemesanan

--- app/src/main/java/com/pnj/bus_ticket_booking/pemesan/AddBookingActivity.kt
+++ app/src/main/java/com/pnj/bus_ticket_booking/pemesan/AddBookingActivity.kt
@@ -1,12 +1,63 @@
 package com.pnj.bus_ticket_booking.pemesan
 
+import android.app.Activity
+import android.app.DatePickerDialog
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import android.util.Log
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.storage.FirebaseStorage
 import com.pnj.bus_ticket_booking.R
+import com.pnj.bus_ticket_booking.admin.TicketActivity
+import com.pnj.bus_ticket_booking.databinding.ActivityAddBookingBinding
+import com.pnj.bus_ticket_booking.databinding.ActivityAddTicketBinding
+import java.text.SimpleDateFormat
+import java.util.*
 
 class AddBookingActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityAddBookingBinding
+    private lateinit var firebaseAuth: FirebaseAuth
+    private val firestoreDatabase = FirebaseFirestore.getInstance()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_add_booking)
+
+        binding = ActivityAddBookingBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.BtnAddBooking.setOnClickListener {
+            addBooking()
+        }
+    }
+
+    fun addBooking() {
+        var nama_pemesan: String = binding.txtNamaPemesan.text.toString()
+        var jumlah_tiket: String = binding.txtJumlahTiket.text.toString()
+
+        val intent = intent
+        val nama_tiket = intent.getStringExtra("nama_tiket").toString()
+        val tanggal_keberangkatan = intent.getStringExtra("tanggal_keberangkatan").toString()
+        val gambar_bus = intent.getStringExtra("gambar_bus").toString()
+
+        val booking: MutableMap<String, Any> = HashMap()
+        booking["nama_pemesan"] = nama_pemesan
+        booking["jumlah_tiket"] = jumlah_tiket
+        booking["nama_tiket"] = nama_tiket
+        booking["tanggal_keberangkatan"] = tanggal_keberangkatan
+        booking["gambar_bus"] = gambar_bus
+
+        val user = firebaseAuth.currentUser
+        val uid = user?.uid
+        Log.e("Uid", uid!!)
+
+        firestoreDatabase.collection("user").document(uid.toString()).collection("booking").add(booking)
+            .addOnSuccessListener {
+                val intentMain = Intent(this, CatalogActivity::class.java)
+                startActivity(intentMain)
+            }
     }
 }
--- app/src/main/java/com/pnj/bus_ticket_booking/pemesan/CatalogActivity.kt
+++ app/src/main/java/com/pnj/bus_ticket_booking/pemesan/CatalogActivity.kt
@@ -1,12 +1,133 @@
 package com.pnj.bus_ticket_booking.pemesan
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.ItemTouchHelper
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.*
 import com.pnj.bus_ticket_booking.R
+import com.pnj.bus_ticket_booking.admin.AddTicketActivity
+import com.pnj.bus_ticket_booking.admin.Ticket
+import com.pnj.bus_ticket_booking.admin.TicketActivity
+import com.pnj.bus_ticket_booking.admin.TicketAdapter
+import com.pnj.bus_ticket_booking.chat.ChatActivity
+import com.pnj.bus_ticket_booking.databinding.ActivityCatalogBinding
+
 
 class CatalogActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityCatalogBinding
+
+    private lateinit var catalogRecyclerView: RecyclerView
+    private lateinit var catalogArrayList: ArrayList<Catalog>
+    private lateinit var catalogAdapter: CatalogAdapter
+    private lateinit var db: FirebaseFirestore
+    private lateinit var firebaseAuth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_catalog)
+
+        binding = ActivityCatalogBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        catalogRecyclerView = binding.ticketListView
+        catalogRecyclerView.layoutManager = LinearLayoutManager(this)
+        catalogRecyclerView.setHasFixedSize(true)
+
+        catalogArrayList = arrayListOf()
+        catalogAdapter = CatalogAdapter(catalogArrayList)
+
+        catalogRecyclerView.adapter = catalogAdapter
+
+        load_data()
+
+        binding.txtSearchTiket.addTextChangedListener(object: TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                val keyword = binding.txtSearchTiket.text.toString()
+                if (keyword.isNotEmpty()){
+                    search_data(keyword)
+                }
+                else{
+                    load_data()
+                }
+            }
+
+            override fun afterTextChanged(p0: Editable?) {}
+        })
+
+        binding.bottomNavigation.setOnItemSelectedListener {
+            when(it.itemId) {
+                R.id.nav_bottom_tiket -> {
+                    val intent = Intent(this, CatalogActivity::class.java)
+                    startActivity(intent)
+                }
+                R.id.nav_bottom_booking -> {
+                    val intent = Intent(this, CatalogActivity::class.java)
+                    startActivity(intent)
+                }
+                R.id.nav_bottom_diskusi -> {
+                    val intent = Intent(this, ChatActivity::class.java)
+                    startActivity(intent)
+                }
+                R.id.nav_bottom_logout -> {
+
+                }
+            }
+            true
+        }
+
+
     }
+
+    private fun load_data() {
+        catalogArrayList.clear()
+        db = FirebaseFirestore.getInstance()
+        db.collection("tiket").
+        addSnapshotListener(object: EventListener<QuerySnapshot> {
+            override fun onEvent(
+                value: QuerySnapshot?,
+                error: FirebaseFirestoreException?
+            ) {
+                if(error != null){
+                    Log.e("Firestore Error", error.message.toString())
+                    return
+                }
+                for(dc: DocumentChange in value?.documentChanges!!){
+                    if(dc.type == DocumentChange.Type.ADDED)
+                        catalogArrayList.add(dc.document.toObject(Catalog::class.java))
+                }
+                catalogAdapter.notifyDataSetChanged()
+            }
+        })
+    }
+
+    private fun search_data(keyword: String) {
+        catalogArrayList.clear()
+
+        db = FirebaseFirestore.getInstance()
+
+        val query = db.collection("tiket")
+            .orderBy("nama_bus")
+            .startAt(keyword)
+            .get()
+        query.addOnSuccessListener {
+            catalogArrayList.clear()
+            for (document in it) {
+                catalogArrayList.add(document.toObject(Catalog::class.java))
+            }
+        }
+    }
+
+
+
 }
